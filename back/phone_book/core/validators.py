ed1df8baa0757e52fa937236be76575225fe64f4
Sergey Smirnov <kurchatov1996@yandex.ru>
2018-07-24 20:53:12 +0300

    fix

--- back/phone_book/core/validators.py
+++ back/phone_book/core/validators.py
@@ -3,5 +3,5 @@ from rest_framework.serializers import ValidationError
 
 
 def phone_validator(val):
-    if not re.match("^\\+79[0-9]{9}$", val):
+    if not re.match("^\\+79[0-9]{9}$", val) and val:
         raise ValidationError('Wrong format')
